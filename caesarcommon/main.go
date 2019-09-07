4dae5cdb41bb259aee991e905ebf792d0ee86de6
EmbraceUU <1014435409@qq.com>
2019-09-07 18:33:28 +0800

    改成go.mod

--- caesarcommon/main.go
+++ caesarcommon/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"caesar-go/caesartest/bitmex"
+	"fmt"
+)
 
+func main() {
+	//lasa := []string{
+	//    "aaa",
+	//    "bbb",
+	//    "ccc",
+	//    "ddd",
+	//    "eee",
+	//    "fff",
+	//    "ggg",
+	//    "hhh",
+	//    "iii",
+	//    "jjj",
+	//    "kkk",
+	//}
+	//for index, l := range lasa {
+	//    fmt.Println(l, " item")
+	//    index = index + 2
+	//}
+
+	//for index := 0; index < len(lasa); index++{
+	//    fmt.Println(lasa[index], " item")
+	//    index = index + 2
+	//}
+
+	//maps := make(map[string][]int)
+	//maps["aaa"] = []int{123, 234}
+	//fmt.Println(maps)
+	intss := make([]int, 2)
+	fmt.Println(intss)
+
+	bitmex.UpdateIN(intss, 3)
+	fmt.Println(intss)
 }
--- caesartest/bitmex/bitmex.go
+++ caesartest/bitmex/bitmex.go
@@ -5,3 +5,7 @@ import caesar "caesar-go/caesartest"
 type Bitmex struct {
 	caesar.Base
 }
+
+func UpdateIN(aaa []int, aa int) {
+	aaa[0] = aa
+}
