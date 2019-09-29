5e2154e88e188baa3c2c775bcfca8edbcf4dc33e
EmbraceUU <1014435409@qq.com>
2019-09-29 11:40:07 +0800

    -

--- main.go
+++ main.go
@@ -1,47 +1,7 @@
 package main
 
-import (
-	_map "caesar-go/note/map"
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	//lasa := []string{
-	//    "aaa",
-	//    "bbb",
-	//    "ccc",
-	//    "ddd",
-	//    "eee",
-	//    "fff",
-	//    "ggg",
-	//    "hhh",
-	//    "iii",
-	//    "jjj",
-	//    "kkk",
-	//}
-	//for index, l := range lasa {
-	//    fmt.Println(l, " item")
-	//    index = index + 2
-	//}
-
-	//for index := 0; index < len(lasa); index++{
-	//    fmt.Println(lasa[index], " item")
-	//    index = index + 2
-	//}
-
-	//maps := make(map[string][]int)
-	//maps["aaa"] = []int{123, 234}
-	//fmt.Println(maps)
-
-	//intss := make([]int, 2)
-	//fmt.Println(intss)
-	//
-	//child.UpdateIN(intss, 3)
-	//fmt.Println(intss)
-
-	testMap := make(map[string]string)
-	testMap["a"] = "aaaa"
-	testMap["b"] = "bbbb"
-	_map.TestMap(testMap)
-	fmt.Println(testMap)
+	fmt.Println("Welcome my friends")
 }
