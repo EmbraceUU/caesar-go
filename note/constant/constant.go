255c652e8083cfd81f70269164bab316e221361f
EmbraceUU <1014435409@qq.com>
2019-09-29 11:54:39 +0800

    增加constant笔记的test

--- note/constant/constant.go
+++ note/constant/constant.go
@@ -1,17 +1,13 @@
 package constant
 
 const (
-	SymbolStatus_Unopened = iota
-	SymbolStatus_Open
-	SymbolStatus_White
-	SymbolStatus_Closed
-	SymbolStatus_Black
+	SymbolstatusUnopened = iota
+	SymbolstatusOpen
+	SymbolstatusWhite
+	SymbolstatusClosed
+	SymbolstatusBlack
 )
 
-const (
-	SymbolStatus_Unopened1 = iota
-	SymbolStatus_Open1
-	SymbolStatus_White1
-	SymbolStatus_Black1
-	SymbolStatus_Closed1
-)
+func GetConstant(constantKey int) int {
+	return constantKey
+}
