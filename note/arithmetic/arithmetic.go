c21abc79f77ed467986ef90bd965cedbe93e7f08
EmbraceUU <1014435409@qq.com>
2020-01-19 15:56:55 +0800

    findMedianSortedArrays

--- note/arithmetic/arithmetic.go
+++ note/arithmetic/arithmetic.go
@@ -1,7 +1,5 @@
 package arithmetic
 
-import "strconv"
-
 /*
 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
@@ -26,6 +24,5 @@ func Reverse(x int) int {
 		}
 		rev = rev*10 + pop
 	}
-	strconv.Atoi()
 	return rev
 }
