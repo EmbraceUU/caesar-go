2e881ba55f00b63fcd01febbf60410e44dac4ed9
EmbraceUU <1014435409@qq.com>
2020-04-13 22:12:16 +0800

    change package name note to awesome

--- awesome/map/map.go
+++ awesome/map/map.go
@@ -18,7 +18,7 @@ func WriteMap(key string, data *AAA) {
 	TMap[key] = data
 }
 
-// note
+// awesome
 // 如果用map作为cache, 那么返回数据时不要直接将map返回
 // 应该生成一个新的map返回出去,
 // 因为如果外部的调用方进行delete操作, 是直接操作的cache map
