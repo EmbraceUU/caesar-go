8440d8ea6bc392667a1ffb6a0f9cf67830291da3
EmbraceUU <1014435409@qq.com>
2020-06-13 21:54:20 +0800

    学习channel

--- awesome/channel/channel_test.go
+++ awesome/channel/channel_test.go
@@ -5,3 +5,39 @@ import "testing"
 func TestWaitLoad(t *testing.T) {
 	WaitLoad()
 }
+
+func TestAction01(t *testing.T) {
+	Action01()
+}
+
+func TestAction02(t *testing.T) {
+	Action02()
+}
+
+func TestAction03(t *testing.T) {
+	Action03()
+}
+
+func TestAction04(t *testing.T) {
+	Action04()
+}
+
+func TestAction05(t *testing.T) {
+	Action05()
+}
+
+func TestAction06(t *testing.T) {
+	Action06()
+}
+
+func TestAction07(t *testing.T) {
+	Action07()
+}
+
+func TestAction08(t *testing.T) {
+	Action08()
+}
+
+func TestAction09(t *testing.T) {
+	Action09()
+}
