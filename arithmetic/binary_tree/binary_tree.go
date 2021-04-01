996fcb3a728c76438b133558a83fd246ef096aea
EmbraceUU <1014435409@qq.com>
2021-04-01 10:06:19 +0800

    学习 BFS和DFS

--- arithmetic/binary_tree/binary_tree.go
+++ arithmetic/binary_tree/binary_tree.go
@@ -7,7 +7,7 @@ import (
 // 以根访问顺序决定是什么遍历
 // 左子树都是优先右子树
 
-// 前序遍历 - 递归
+// 【前序遍历 递归】
 func preorderTraversal(root *TreeNode) {
 	if root == nil {
 		return
@@ -20,8 +20,8 @@ func preorderTraversal(root *TreeNode) {
 	preorderTraversal(root.Right)
 }
 
-// 前序遍历 - 非递归
-func preorderTraversal2(root *TreeNode) {
+// 【前序遍历 非递归】
+func preorderTraversalII(root *TreeNode) {
 	if root == nil {
 		return
 	}
@@ -43,7 +43,7 @@ func preorderTraversal2(root *TreeNode) {
 	}
 }
 
-// 中序遍历 - 非递归
+// 【中序遍历 非递归】
 // 左子树 -> 根节点 -> 右子树
 func inorderTraversal(root *TreeNode) {
 	if root == nil {
@@ -66,7 +66,7 @@ func inorderTraversal(root *TreeNode) {
 	}
 }
 
-// 后序遍历
+// 【后序遍历 非递归】
 // 左子树 -> 右子树 -> 根节点
 func postorderTraversal(root *TreeNode) []int {
 	if root == nil {
@@ -99,8 +99,11 @@ func postorderTraversal(root *TreeNode) []int {
 	return result
 }
 
-// DFS 深度遍历 从下到上 分治法
-func preorderTraversal3(root *TreeNode) []int {
+// 【DFS 深度遍历 分治法】
+// DFS中 关键点是【递归以及回溯】
+// 采用递归的方式, 先分别递归返回结果, 然后合并结果, 合并的时候, 可以控制访问的顺序
+// 可以简单的实现 preorder/inorder/postorder 这三种顺序
+func preorderTraversalIII(root *TreeNode) []int {
 	result := divideAndConquer(root)
 	return result
 }
@@ -117,5 +120,70 @@ func divideAndConquer(root *TreeNode) []int {
 	result = append(result, root.Val)
 	result = append(result, left...)
 	result = append(result, right...)
+	return result
+}
+
+// 【BFS 广度优先搜索 第一版】
+func levelOrder(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return nil
+	}
+
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+
+	for len(queue) > 0 {
+		var level []int
+		var queueTemp []*TreeNode
+		for i := range queue {
+			level = append(level, queue[i].Val)
+			if queue[i].Left != nil {
+				queueTemp = append(queueTemp, queue[i].Left)
+			}
+			if queue[i].Right != nil {
+				queueTemp = append(queueTemp, queue[i].Right)
+			}
+		}
+		result = append(result, level)
+		queue = queueTemp
+	}
+
+	return result
+}
+
+// 【BFS 广度优先搜索 改进版】
+// 遍历当前层，并标记下一层, 然后再遍历下一层, 以此循环遍历
+// TODO 为什么改进版比第一版的内存消耗更大了 ？
+func levelOrderII(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return nil
+	}
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	// 遍历当前层
+	for len(queue) > 0 {
+		// 当前level遍历的结果
+		list := make([]int, 0)
+		// 先得到当前queue的长度，遍历当前层，再添加下一层
+		l := len(queue)
+		// 只遍历预先固定的长度, 这样不需要再每次创建queueTemp~~
+		for i := 0; i < l; i++ {
+			// 每次将队首出列, 先进先出, 这样即使queue变化了, 也不影响下面的继续访问
+			level := queue[0]
+			queue = queue[1:]
+
+			list = append(list, level.Val)
+			if level.Left != nil {
+				queue = append(queue, level.Left)
+			}
+			if level.Right != nil {
+				queue = append(queue, level.Right)
+			}
+		}
+		// 遍历完当前层, 将结果集合并到result中
+		result = append(result, list)
+	}
 	return result
 }
