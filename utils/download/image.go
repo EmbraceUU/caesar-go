5e318f6b6abdfd34e908270d3f3c66be27056fb3
EmbraceUU <1014435409@qq.com>
2021-04-14 10:18:03 +0800

    示例 下载图片

---
+++ utils/download/image.go
@@ -0,0 +1,38 @@
+package download
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type Image struct {
+	imagPath string
+	name     string
+}
+
+func NewImage(imagePath, name string) *Image {
+	return &Image{
+		imagPath: imagePath,
+		name:     name,
+	}
+}
+
+func (i *Image) Download() error {
+	resp, err := http.Get(i.imagPath)
+	if err != nil {
+		return err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	out, err := os.Create(i.name)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(out, bytes.NewReader(body))
+	return err
+}
