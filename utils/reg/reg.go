074bbec7cf4a25a96455acef1718eef259c55300
EmbraceUU <1014435409@qq.com>
2022-01-04 22:23:07 +0800

    utils::增加reg的例子

--- utils/reg/reg.go
+++ utils/reg/reg.go
@@ -1,15 +1,15 @@
-package str
+package reg
 
 import (
 	"regexp"
 )
 
-// 清除字符串中的数字
+// Reg 清除字符串中的数字
 func Reg(key string) string {
 	return regexp.MustCompile(`[0-9]+$`).ReplaceAllString(key, "")
 }
 
-// 清除字符串中, 从数字开始匹配的后面的字符
+// RegSub 清除字符串中, 从数字开始匹配的后面的字符
 func RegSub(key string) string {
 	return regexp.MustCompile(`[0-9]+.*$`).ReplaceAllString(key, "")
 }
--- utils/reg/reg_test.go
+++ utils/reg/reg_test.go
@@ -1,4 +1,4 @@
-package str
+package reg
 
 import "testing"
 
